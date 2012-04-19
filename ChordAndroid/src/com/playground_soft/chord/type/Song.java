468126e84da2b0cb66840287f57b91a4ebc6ef40
Noom <mr_tawan@hotmail.com>
2012-04-20 06:05:30 +0700

    * missing files

---
+++ ChordAndroid/src/com/playground_soft/chord/type/Song.java
@@ -0,0 +1,23 @@
+package com.playground_soft.chord.type;
+
+import java.io.File;
+
+public class Song {
+    public final String name;
+    public final String artist;
+    public final File file;
+    public final int id;
+    
+    
+    public Song(String name, String artist, File file, int id) {
+        this.name = name;
+        this.artist = artist;
+        this.file = file;
+        this.id = id;
+    }
+    
+    @Override
+    public String toString() {
+        return name + "//"+artist;
+    }
+}
